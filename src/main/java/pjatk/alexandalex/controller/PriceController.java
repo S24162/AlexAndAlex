8617804b1aed7b4448450158812c9de0725a063e
Egrakovitch <DakarKTM690r@gmail.com>
2025-09-04 06:30:46 +0200

    small corrections, adding Thymeleaf note to section head

--- src/main/java/pjatk/alexandalex/controller/PriceController.java
+++ src/main/java/pjatk/alexandalex/controller/PriceController.java
@@ -32,13 +32,6 @@ public class PriceController {
    @ResponseBody
    public Map<String, Object> calculate(@RequestBody KitchenMeasurementDTO dto) {
       BigDecimal price = kitchenMeasurementService.calculatePrice(dto);
-      System.out.println(dto.getColorId());
-      System.out.println(dto.getMaterialId());
-      System.out.println(dto.getMechanismId());
-      System.out.println(price);
-
-
-//      System.out.println("price controller returns: " + price);
       return Map.of("price", price);
    }
 
